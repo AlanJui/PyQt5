6202332286c85d86d2bf1eec412c8de04482e02b
Alan Jui <AlanJui.1960@gmail.com>
2024-01-10 23:09:16 +0800

    建置專案基礎環境

---
+++ pg001.py
@@ -0,0 +1,17 @@
+import sys
+
+from PyQt5 import QtCore, QtWidgets
+
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+MainWindow.setObjectName("MainWindow")
+MainWindow.setWindowTitle("oxxo.studio")
+MainWindow.resize(300, 200)
+
+pushButton = QtWidgets.QPushButton(MainWindow)
+pushButton.setGeometry(QtCore.QRect(100, 70, 113, 32))
+pushButton.setObjectName("pushButton")
+pushButton.setText("PushButton")
+
+MainWindow.show()
+sys.exit(app.exec_())
